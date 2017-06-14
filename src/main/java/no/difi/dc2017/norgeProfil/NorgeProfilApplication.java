f6d4114657b8602644f627b22d4bcb156ead0c92
Skar <camp-isk@DIFI.no>
2017-06-14 10:56:36 +0200

    initial commit

---
+++ src/main/java/no/difi/dc2017/norgeProfil/NorgeProfilApplication.java
@@ -0,0 +1,12 @@
+package no.difi.dc2017.norgeProfil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorgeProfilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorgeProfilApplication.class, args);
+	}
+}
---
+++ src/test/java/no/difi/dc2017/norgeProfil/NorgeProfilApplicationTests.java
@@ -0,0 +1,16 @@
+package no.difi.dc2017.norgeProfil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NorgeProfilApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
