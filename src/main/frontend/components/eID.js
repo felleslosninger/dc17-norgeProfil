4c6b7fee3413494fd5307797ea957d4cce41e615
tranebear <tcamillat@gmail.com>
2017-06-21 10:47:41 +0200

    Added list

--- src/main/frontend/components/eID.js
+++ src/main/frontend/components/eID.js
@@ -4,7 +4,7 @@ var Row = require('react-bootstrap/lib/Row');
 var Col = require('react-bootstrap/lib/Col');
 
 import {Card, CardHeader, CardText} from 'material-ui/Card';
-
+import {List, ListItem} from 'material-ui/List';
 
 class EID extends React.Component {
 
@@ -21,8 +21,31 @@ class EID extends React.Component {
                     </div>
                 </Row>
                 <CardText className="CardText">
-                    minID
-                    bankID
+                    <div>
+                        <List>
+                            <ListItem primaryText="Sent mail" />
+                            <ListItem primaryText="Drafts" />
+                            <ListItem
+                                primaryText="Inbox"
+                                initiallyOpen={true}
+                                primaryTogglesNestedList={true}
+                                nestedItems={[
+                                    <ListItem
+                                        key={1}
+                                        primaryText="Starred"
+                                    />,
+                                    <ListItem
+                                        key={2}
+                                        primaryText="Sent Mail"
+                                    />,
+                                    <ListItem
+                                        key={3}
+                                        primaryText="Inbox"
+                                    />,
+                                ]}
+                            />
+                        </List>
+                    </div>
                 </CardText>
             </Card>
         )
