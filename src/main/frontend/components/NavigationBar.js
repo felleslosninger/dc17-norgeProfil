032b4a73b6c9a4bbe65e52a71f231c4e9c96feae
Tran <camp-ctr@DIFI.no>
2017-07-07 14:38:35 +0200

    Added side menu

--- src/main/frontend/components/NavigationBar.js
+++ src/main/frontend/components/NavigationBar.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import {AppBar, Tabs, Tab} from 'material-ui'
+import {MenuItem} from 'material-ui'
+import Drawer from 'material-ui/Drawer';
 
 class NavigationBar extends React.Component {
 
     constructor(props) {
         super(props);
-
+        this.state = {open: false};
     }
+
+    handleToggle = () => this.setState({open: !this.state.open});
 
 
     render() {
 
         return (
-            <AppBar title="Digital Borger">
-                <Tabs>
-                    <Tab label="Profil" />
-                    <Tab label="Logg ut" href="/logout" />
-                </Tabs>
-            </AppBar>
+            <div>
+                <AppBar title="Digital Borger" onLeftIconButtonTouchTap={this.handleToggle}>
+                    <Tabs>
+                        <Tab label="Profil" />
+                        <Tab label="Logg ut" href="/logout" />
+                    </Tabs>
+                </AppBar>
+                <Drawer containerStyle={{height: 'calc(100% - 64px)', top: 64}} open={this.state.open}>
+                    <MenuItem>Menu Item</MenuItem>
+                    <MenuItem>Menu Item 2</MenuItem>
+                </Drawer>
+            </div>
+
+
         );
     }
 }
