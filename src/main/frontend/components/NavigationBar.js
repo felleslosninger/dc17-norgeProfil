ce2005df5f09e65720dfa0e9c7b6a0cf3c8751be
Norstein <andrnors@stud.ntnu.no>
2017-07-07 12:05:36 +0200

    added login and logout to navbar

--- src/main/frontend/components/NavigationBar.js
+++ src/main/frontend/components/NavigationBar.js
@@ -1,24 +1,68 @@
 import React from 'react'
 import {AppBar, Tabs, Tab} from 'material-ui'
+import axios from "axios";
 
 class NavigationBar extends React.Component {
 
     constructor(props) {
         super(props);
-
+        this.state = {
+          status: false,
+        };
+        this.isLoggedIn = this.isLoggedIn.bind(this);
     }
 
 
+    componentWillMount() {
+        this.isLoggedIn();
+    }
+
+    isLoggedIn = () => {
+        let stat = false;
+        console.log("Tulling");
+        axios.get('/user')
+            .then((response) => {
+                if (response.data == "") {
+                    this.setState({
+                        status: false
+                    });
+                } else {
+                    if (response.data.authenticated == true) {
+                        this.setState({
+                            status: true
+                        });
+                    }
+                    else {
+                        this.setState({
+                            status: false
+                        });
+                    }
+                }
+            });
+    };
+
     render() {
 
-        return (
-            <AppBar title="Digital Borger">
-                <Tabs>
-                    <Tab label="Profil" />
-                    <Tab label="Logg ut" href="/logout" />
-                </Tabs>
-            </AppBar>
-        );
+        if (this.state.status) {
+            console.log("Logget inn");
+            return (
+                <AppBar title="Digital Borger">
+                    <Tabs>
+                        <Tab label="Profil "/>
+                        <Tab label="Logg ut" href="/logout"/>
+                    </Tabs>
+                </AppBar>
+            );
+        } else {
+            console.log("Logget ut");
+            return (
+                <AppBar title="Digital Borger">
+                    <Tabs>
+                        <Tab label="Logg inn" href="/login/idporten"/>
+                    </Tabs>
+                </AppBar>
+            );
+        }
     }
 }
 
