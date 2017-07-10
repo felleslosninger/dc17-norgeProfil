67abf367e89ef18f643256a28bb6204fbd81b279
Svendsen <camp-cms@DIFI.no>
2017-07-10 09:06:50 +0200

    fix responsive layout

--- src/main/frontend/components/ContactInfoCard.js
+++ src/main/frontend/components/ContactInfoCard.js
@@ -83,8 +83,8 @@ class ContactInfoCard extends React.Component {
         return (
             <Card className="Card">
                 <Row className="CardHeader">
-                    <Col sm={3} lg={2}  className="Icon">{<ActionInfo />}</Col>
-                    <Col sm={3} lg={7}><h4>Kontaktinformasjon</h4></Col>
+                    <Col sm={3} md={2} lg={2}  className="Icon">{<ActionInfo />}</Col>
+                    <Col sm={8} md={10} lg={10}><h4>Kontaktinformasjon</h4></Col>
                 </Row>
                 <hr className="HLine"/>
                 <CardText>
