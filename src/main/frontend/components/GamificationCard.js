489152d27fe250009f788189c9501e0fdaa484c7
Skar <ingrskar@gmail.com>
2017-07-19 11:01:44 +0200

    placed facebook share btn

--- src/main/frontend/components/GamificationCard.js
+++ src/main/frontend/components/GamificationCard.js
@@ -2,83 +2,79 @@ var Row = require('react-bootstrap/lib/Row');
 var Col = require('react-bootstrap/lib/Col');
 var Grid = require('react-bootstrap/lib/Grid');
 
-import { Progress } from 'react-sweet-progress';
-
+import {Progress} from 'react-sweet-progress';
+import FacebookShare from '../components/FacebookShare';
 import React, {Component} from 'react';
 import {Card, CardText} from 'material-ui/Card';
 import GameLog from './GameLog';
 
 class GamificationCard extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
 
-    calcBarColor(value){
-        let h = Math.floor(120*value/100);
+    calcBarColor(value) {
+        let h = Math.floor(120 * value / 100);
         return "hsl(" + h + ",100%, 50%)";
     }
 
-    calcCircleColor(value){
-        let s = 25 + Math.floor(0.75*value);
-        return "hsl( 192 ,"+ s +"%, 50%)";
+    calcCircleColor(value) {
+        let s = 25 + Math.floor(0.75 * value);
+        return "hsl( 192 ," + s + "%, 50%)";
     }
 
-    render(){
-        return(
-            <div className="gamification">
-                <Card>
-                    <div className="gamificationContainer">
-                        <Row>
-                            <Col lg={6}>
-                                <div className = "ProgressCircle">
-                                    <Progress
-                                        width = {200}
-                                        strokeWidth={10}
-                                        status = "active"
-                                        type="circle"
-                                        percent={this.props.gameState.levelProgress}
-                                        theme={{
-                                            active: {
-                                                symbol: "Nivå: " + this.props.gameState.level,
-                                                color: this.calcCircleColor(this.props.gameState.levelProgress),
-                                            },
-                                            default: {
-                                                symbol: "Nivå: " + this.props.gameState.level,
-                                            }
-                                        }}
-                                    />
-                                </div>
-                            </Col>
-                            <Col lg={6}>
-                                <GameLog pointList = {this.props.pointList}/>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg={12}>
-                                <div className = "ProgressBar">
-                                    <Progress
-                                        status = "active"
-                                        width = {200}
-                                        strokeWidth={10}
-                                        percent={this.props.gameState.percentage}
-                                        theme={{
-                                            active: {
-                                                symbol: this.props.gameState.points + "/" + this.props.gameState.maxScore + " Poeng",
-                                                color: this.calcBarColor(this.props.gameState.percentage),
-                                            },
-                                            default: {
-                                                symbol: this.props.gameState.points + "/" + this.props.gameState.maxScore + " Poeng",
-                                            }
-                                        }}
-                                    />
-                                </div>
-                            </Col>
-                        </Row>
+    render() {
+        return (
+            <Card className="gamification">
+                <div className="gamificationContainer">
+                    <Row>
+                        <Col lg={6}>
+                            <div className="ProgressCircle">
+                                <Progress
+                                    width={200}
+                                    strokeWidth={10}
+                                    status="active"
+                                    type="circle"
+                                    percent={this.props.gameState.levelProgress}
+                                    theme={{
+                                        active: {
+                                            symbol: "Nivå: " + this.props.gameState.level,
+                                            color: this.calcCircleColor(this.props.gameState.levelProgress),
+                                        },
+                                        default: {
+                                            symbol: "Nivå: " + this.props.gameState.level,
+                                        }
+                                    }}
+                                />
+                            </div>
+                        </Col>
+                        <Col lg={6}>
+                            <GameLog pointList={this.props.pointList}/>
+                        </Col>
+                    </Row>
+                    <div className="ProgressBar">
+                        <Progress
+                            status="active"
+                            width={200}
+                            strokeWidth={10}
+                            percent={this.props.gameState.percentage}
+                            theme={{
+                                active: {
+                                    symbol: this.props.gameState.points + "/" + this.props.gameState.maxScore + " Poeng",
+                                    color: this.calcBarColor(this.props.gameState.percentage),
+                                },
+                                default: {
+                                    symbol: this.props.gameState.points + "/" + this.props.gameState.maxScore + " Poeng",
+                                }
+                            }}
+                        />
                     </div>
-                </Card>
-            </div>
+
+                <FacebookShare className="facebook" score={this.props.gameState}/>
+                </div>
+            </Card>
         );
     }
 
