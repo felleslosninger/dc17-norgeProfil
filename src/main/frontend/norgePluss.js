46198ef48467e9aee313287f85b5973926ad5d6a
oleob <oleob@stud.ntnu.no>
2017-07-25 13:55:58 +0200

    removed test containers

--- src/main/frontend/norgePluss.js
+++ src/main/frontend/norgePluss.js
@@ -5,27 +5,9 @@ import configureStore from "./utilities/store";
 import NorgePlussContainer from './norgePluss/containers/NorgePlussContainer';
 import HeaderContainer from './norgePluss/containers/HeaderContainer';
 import GamificationContainer from './norgePluss/containers/GamificationContainer'
-import TestContainer from './norgePluss/containers/TestContainer';
-import TestContainer2 from './norgePluss/containers/TestContainer2';
 import {Provider} from "react-redux";
 
 const store = configureStore();
-
-const testPluss = () => {
-    ReactDOM.render(
-        <Provider store = {store}>
-            <TestContainer/>
-        </Provider>,
-        document.getElementById('boxList')
-        );
-
-    ReactDOM.render(
-        <Provider store = {store}>
-            <TestContainer2/>
-        </Provider>,
-        document.getElementById('header')
-    );
-}
 
 const norgePluss = () => {
 
